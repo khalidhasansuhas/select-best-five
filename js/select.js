d197647cfb3fdbd31b7fd7c204fe3687b178c053
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-08-22 23:05:42 +0600

    Started JavaScript

---
+++ js/select.js
@@ -0,0 +1,5 @@
+document.getElementById('select-messi').addEventListener('click', function(){
+    const playersName = document.getElementById('player-messi');
+    const player = playersName.innerText;
+    console.log(player);
+})
