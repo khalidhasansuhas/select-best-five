9223a96dd4aff7d812284ac90e9151e703caf45c
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-08-23 11:45:46 +0600

    first utility added and alert message added

--- js/select.js
+++ js/select.js
@@ -1,4 +1,24 @@
 document.getElementById('select-messi').addEventListener('click', function(){
-    getSelectedPlayerNameByUsingId('select-messi','player messi');
+selectByClick('player-messi', 'select-messi');
+
+})
+document.getElementById('select-neymar').addEventListener('click', function(){
+selectByClick('player-neymar','select-neymar');
+
+})
+document.getElementById('select-mbappe').addEventListener('click', function(){
+selectByClick('player-mbappe','select-mbappe');
+
+})
+document.getElementById('select-vitor').addEventListener('click', function(){
+selectByClick('player-vitor','select-vitor');
+
+})
+document.getElementById('select-ramos').addEventListener('click', function(){
+selectByClick('player-renato','select-ramos');
+
+})
+document.getElementById('select-renato').addEventListener('click', function(){
+selectByClick('player-renato','select-renato');
 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,14 +1,36 @@
-function getSelectedPlayerNameByUsingId(elemntId, playerName){
-    //let player = document.getElementById(playerName).innerText;
-    let playerInList = document.getElementById('selected-player').childElementCount;
-    if(playerInList < 5){
-        const newList = document.createElement('li');
-        const newListText = document.createTextNode(playerName);
-        newList.appendChild(newListText);
+// function getSelectedPlayerNameByUsingId(elemntId, playerName){
+//     //let player = document.getElementById(playerName).innerText;
+//     let playerInList = document.getElementById('selected-player').childElementCount;
+//     if(playerInList < 5){
+//         const newList = document.createElement('li');
+//         const newListText = document.createTextNode(playerName);
+//         newList.appendChild(newListText);
 
-        document.getElementById(elemntId).disbled = true;
+//         document.getElementById(elemntId).disbled = true;
+//     }
+//     else {
+//         alert('Maximum Players Added');
+//     }
+// }
+
+function selectByClick(selectPlayerByClick, player){
+    const selectPlayer = document.getElementById(selectPlayerByClick).innerText;
+
+    let playerInList = document.getElementById('selected-player');
+    let playerNode = playerInList.childElementCount;
+    console.log(playerNode);
+
+    if (playerNode < 5 ){
+        const playerList = document.getElementById('selected-player');
+        const newList = document.createElement('li');
+        newList.innerText = selectPlayer;
+        playerList.appendChild(newList);
+
+        document.getElementById(player).disabled = true ;
     }
     else {
-        alert('Maximum Players Added');
+        alert ('maximum player added');
     }
+
+    
 }
